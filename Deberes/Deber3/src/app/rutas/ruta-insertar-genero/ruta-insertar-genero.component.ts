047b485a944f5ad46dd72e272f4ee5c1006228a8
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-02-14 23:16:28 -0500

    Feature / Deber 3

---
+++ Deberes/Deber3/src/app/rutas/ruta-insertar-genero/ruta-insertar-genero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-insertar-genero',
+  templateUrl: './ruta-insertar-genero.component.html',
+  styleUrls: ['./ruta-insertar-genero.component.css']
+})
+export class RutaInsertarGeneroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
